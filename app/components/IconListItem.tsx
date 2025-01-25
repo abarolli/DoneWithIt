adc0126dbb2862abf269b79e83e13c4ff1791bcf
abarolli <obarolli@gmail.com>
2025-01-25 14:46:03 -0700

    Added IconListItem.tsx

---
+++ app/components/IconListItem.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { IconProps } from "@expo/vector-icons/build/createIconSet";
+
+import IconTypes from "../../configs/icons";
+import colors from "../../configs/colors";
+
+interface Props {
+  name: IconTypes;
+  size: number;
+  title: string;
+  color: string;
+  style?: StyleProp<ViewStyle>;
+}
+
+function IconListItem({ name, size, title, color, style }: Props) {
+  const width = size * 2;
+  const height = size * 2;
+
+  return (
+    <TouchableHighlight underlayColor={colors.lightGrey} onPress={() => {}}>
+      <View style={[styles.container, style]}>
+        <View
+          style={[
+            styles.iconContainer,
+            { backgroundColor: color, borderRadius: height / 2, height, width },
+          ]}
+        >
+          <MaterialCommunityIcons name={name} size={size} color="white" />
+        </View>
+        <Text>{title}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 20,
+  },
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 20,
+  },
+});
+
+export default IconListItem;
