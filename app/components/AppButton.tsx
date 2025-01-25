ee63351f175e9a0b4c52c06e457ac84ba27c8f81
abarolli <obarolli@gmail.com>
2025-01-25 14:45:04 -0700

    Added AppButton.tsx

---
+++ app/components/AppButton.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import colors from "../../configs/colors";
+
+interface Props {
+  children: string;
+  color?: string;
+}
+
+function AppButton({ children, color = undefined }: Props) {
+  return (
+    <View
+      style={{
+        ...styles.button,
+        backgroundColor: color || styles.button.backgroundColor,
+      }}
+    >
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    height: 60,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    marginBottom: 20,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+});
+export default AppButton;
