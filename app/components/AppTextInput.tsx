81a00bbaf56938f8e504de0927d81701f6b5c33d
abarolli <obarolli@gmail.com>
2025-01-26 17:40:12 -0700

    Added AppTextInput.tsx

---
+++ app/components/AppTextInput.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  TextInput,
+  TextInputProps,
+  View,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../../configs/colors";
+import IconTypes from "../../configs/icons";
+
+interface Props extends TextInputProps {
+  icon?: IconTypes;
+}
+
+function AppTextInput({ icon, ...otherProps }: Props) {
+  return (
+    <View style={styles.container}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={colors.medium}
+          style={styles.icon}
+        />
+      )}
+      <TextInput style={styles.textInput} {...otherProps} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.lightGrey,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  textInput: {
+    color: colors.medium,
+    fontSize: 18,
+    flex: 1,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
+
+export default AppTextInput;
