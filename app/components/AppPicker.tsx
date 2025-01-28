40d48ab69fa2684f19152d835e94178a5a5e3ce5
abarolli <obarolli@gmail.com>
2025-01-27 17:35:36 -0700

    Added custom Picker in AppPicker.tsx

---
+++ app/components/AppPicker.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import defaultStyles from "../configs/styles";
+import IconTypes from "../configs/icons";
+import colors from "../configs/colors";
+import Screen from "./Screen";
+
+interface Props {
+  icon?: IconTypes;
+  placeholder: string;
+}
+
+function AppPicker({ icon, placeholder }: Props) {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <Text style={styles.text}>{placeholder}</Text>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+        </Screen>
+      </Modal>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.lightGrey,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  text: {
+    flex: 1,
+  },
+  textInput: {
+    ...defaultStyles.text,
+    flex: 1,
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
+
+export default AppPicker;
